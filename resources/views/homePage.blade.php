2f5e879348d16c94a5c9fe9a107e390168b587ea
Al-Giovinetti <giovinettialessio@gmail.com>
2024-11-04 00:35:17 +0100

    done a first draft of home page

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('title','home')
+
+@section('content')
+<div class="container">
+    <section>
+        <div class="text-center">
+            <h1>Immobiliando</h1>
+            <p>Gestisci i tuoi immobili in piena autonomia</p>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <h2 class="text-center my-4">Perchè ci scelgono</h2>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="text-center m-2 p-3 border">
+                    <h3>Immobili</h3>
+                    <p>Aggiungi un immobile alla tua lista e il gioco è fatto</p>
+                </div>    
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="text-center m-2 p-3 border">
+                    <h3>Richieste</h3>
+                    <p>Rendiamo possibile l'incontro tra domanda e offerta</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="text-center m-2 p-3 border">
+                    <h3>Marketing</h3>
+                    <p>Pubblicizziamo i tuoi immobili in tutta Italia</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="text-center m-2 p-3 border 
+                ">
+                    <h3>Aggiornato</h3>
+                    <p>In continuo aggiornamento per garantire il massimo delle prestazioni</p>
+                </div>
+            </div>
+        </div>
+       
+        
+    </section>
+
+    
+</div>
+@endsection
