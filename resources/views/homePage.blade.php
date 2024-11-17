2e563101278a32a8c3f8eb1f2373d85e1be03085
Al-Giovinetti <giovinettialessio@gmail.com>
2024-11-17 16:34:53 +0100

    added texts and correct indentation

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -4,45 +4,55 @@
 
 @section('content')
 <div class="container">
-    <section>
+    <section class="py-5">
         <div class="text-center">
             <h1>Immobiliando</h1>
             <p>Gestisci i tuoi immobili in piena autonomia</p>
         </div>
     </section>
-    <section>
+    <section class="py-4">
+        <h2 class="text-center mb-4">Perchè ci scelgono</h2>
         <div class="row">
-            <h2 class="text-center my-4">Perchè ci scelgono</h2>
-            <div class="col-12 col-md-6 col-lg-3">
-                <div class="text-center m-2 p-3 border">
+            <div class="col-12 col-md-6 col-lg-3 mb-4">
+                <div class="text-center p-3 border h-100">
                     <h3>Immobili</h3>
-                    <p>Aggiungi un immobile alla tua lista e il gioco è fatto</p>
+                    <div class="h-75 d-flex">
+                        <p class="m-auto pb-3">Aggiungi un immobile alla tua lista e il gioco è fatto</p>
+                    </div> 
                 </div>    
             </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                <div class="text-center m-2 p-3 border">
+            <div class="col-12 col-md-6 col-lg-3 mb-4">
+                <div class="text-center p-3 border h-100">
                     <h3>Richieste</h3>
-                    <p>Rendiamo possibile l'incontro tra domanda e offerta</p>
+                    <div class="h-75 d-flex">
+                        <p class="m-auto pb-3">Rendiamo possibile l'incontro tra domanda e offerta</p>
+                    </div>
+                    
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                <div class="text-center m-2 p-3 border">
+            <div class="col-12 col-md-6 col-lg-3 mb-4">
+                <div class="text-center p-3 border h-100">
                     <h3>Marketing</h3>
-                    <p>Pubblicizziamo i tuoi immobili in tutta Italia</p>
+                    <div class="h-75 d-flex">
+                        <p class="m-auto pb-3">Pubblicizziamo i tuoi immobili in tutta Italia</p>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                <div class="text-center m-2 p-3 border 
-                ">
+            <div class="col-12 col-md-6 col-lg-3 mb-4">
+                <div class="text-center p-3 border h-100">
                     <h3>Aggiornato</h3>
-                    <p>In continuo aggiornamento per garantire il massimo delle prestazioni</p>
+                    <div class="h-75 d-flex">
+                        <p class="m-auto pb-3">In continuo aggiornamento per garantire il massimo delle prestazioni</p>
+                    </div>
                 </div>
             </div>
         </div>
-       
-        
     </section>
-
-    
+    <section class="py-4">
+        <h5>Il nostro punto di forza?</h5>
+        <h2 class="mb-4">Assistenza continua</h2>
+        <p>Da oltre 30 anni il nostro team collabora con le agenzie immobiliari, mettendo a frutto l'esperienza acquisita per offrirti una consulenza rapida ed efficace nella gestione di qualsiasi problematica.</p>
+        <p>Nel gestionale avrai la possibilità di richiedere assistenza remota in qualsiasi momento, garantendoti così un supporto tecnico sempre disponibile e di alta qualità.</p>
+    </section>
 </div>
 @endsection
